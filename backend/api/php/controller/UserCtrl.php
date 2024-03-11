6f9da96a1d245d99dc2a2592a1836cd201d96bfa
Jejebal <96423022+Jejebal@users.noreply.github.com>
2024-03-11 11:32:51 +0100

    UserCtrl checkLogin() ajout retour adéquat

--- backend/api/php/controller/UserCtrl.php
+++ backend/api/php/controller/UserCtrl.php
@@ -178,46 +178,6 @@ class UserCtrl {
 
         }
 
-        if($email == "" || strlen($email) < 7 ||strlen($email) > 100 || !$email){
-
-            $error["email"] = "L'email que vous essayer d'ajouter n'est pas valide.";
-
-        }
-        
-        if($motPasse == "" || strlen($motPasse) < 5 || strlen($motPasse) > 50 || !$motPasse){
-
-            $error["motPasse"] = "Le mot de passe que vous essayez d'utiliser n'est pas valide. Il dois être plus grand que 5 et plus petit que 50.";
-
-        }
-
-        if($remediation <= 0 || $remediation > 29){
-
-            $error["remediation"] = "La remediation que vous essayer d'utiliser n'est pas valide. Elle dois être plus grand que 0 et plus petit que 29.";
-
-        }
-
-        if($idGroupe <= 0){
-
-            $error["groupe"] = "Le groupe que vous essayer d'utiliser ne peut pas existez.";
-
-        }
-
-        if(empty($error)){
-
-            $user = new UserModel($init = [ 
-                "nom_utilisateur" => $nom,
-                "email" => $email,
-                "mot_passe" => password_hash($motPasse, PASSWORD_DEFAULT),
-                "remediation" => $remediation,
-                "id_groupe" => $idGroupe
-            ]);
-
-            return $user->creatUser();
-
-        }
-        
-        return $error;
-
     }
 
 }
