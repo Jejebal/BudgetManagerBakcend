9097379825e6c002815c432960e72a2b16e85ca3
Jejebal <96423022+Jejebal@users.noreply.github.com>
2024-03-15 12:56:36 +0100

    Delete backend/secret.php

--- backend/secret.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Constantes liées à la base de données
-define('BDD_HOTE', 'localhost');
-define('BDD_NOM', 'science_scope');
-define('BDD_UTILISATEUR', 'root');
-define('BDD_MOT_DE_PASSE', 'Super');
-define('BDD_CHARSET', 'utf8mb4');
