4dc442e7c17e7f0c03997d452779a7b06442b988
Jejebal <96423022+Jejebal@users.noreply.github.com>
2024-04-19 12:56:58 +0200

    Ajout de l'arborescence C# window form

---
+++ frontend/budgetManager/budgetManager/Form1.cs
@@ -0,0 +1,10 @@
+namespace budgetManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ frontend/budgetManager/budgetManager/classe/Utilisateur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budgetManager.classe
+{
+    internal class Utilisateur
+    {
+    }
+}
