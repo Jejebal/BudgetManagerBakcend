a59f53148a731ffd7cdb9ac20cf604762f7a68c7
Jejebal <96423022+Jejebal@users.noreply.github.com>
2024-04-29 09:03:15 +0200

    Ajout d'une route register
    L'index login a été séparez en deux fichier différent les deux en post. Il y a le login pour vérifier les information de connexion d'un utilisateur et le deuxième register sert a pouvoir s'inscrire dans l'application et créer un utilisateur.

--- backend/api/login/index.php
+++ backend/api/login/index.php
@@ -12,7 +12,7 @@
 
 header('Content-type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-HEADER: *");
+header("Access-Control-Allow-Headers: *");
 
 require_once("../php/constantes.php");
 require_once("../php/fonction.php");
@@ -20,19 +20,19 @@ require_once("../../../vendor/autoload.php");
 require_once("../../secret.php");
 
 use Projet\Budgetmanager\api\php\controller\UserCtrl;
-use Projet\Budgetmanager\api\php\controller\GroupeCtrl;
 
 $error = [];
 $userCtrl = new UserCtrl();
-$groupeCtrl = new GroupeCtrl();
 
-if($_SERVER["REQUEST_METHOD"] == "GET"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $nom = "";
     $motPasse = "";
 
-    $nom = filter_input(INPUT_GET, "nom", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $motPasse = filter_input(INPUT_GET, "motPasse", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $donnees = recuperDonner();
+
+    $nom = array_key_exists("nom", $donnees) ? filter_var($donnees["nom"], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null;
+    $motPasse = array_key_exists("motPasse", $donnees) ? filter_var($donnees["motPasse"], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null;
 
     $user = $userCtrl->checkLogin($nom, $motPasse);
 
@@ -47,71 +47,6 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     else{
 
         http_response_code(RETOURNE_INFORMATION);
-        die();
-
-    }
-
-}
-else if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    $nom = "";
-    $email = "";
-    $motPasse = "";
-    $remediation = 0;
-    $idGroupe = 0;
-
-    $donnees = recuperDonner();
-
-    $nom = array_key_exists("nom", $donnees) ? filter_var($donnees["nom"], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null;
-    $email = array_key_exists("email", $donnees) ? filter_var($donnees["email"], FILTER_VALIDATE_EMAIL) : null;
-    $motPasse = array_key_exists("motPasse", $donnees) ? filter_var($donnees["motPasse"], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null;
-    $remediation = array_key_exists("remediation", $donnees) ? filter_var($donnees["remediation"], FILTER_VALIDATE_INT) : null;
-    $idGroupe = array_key_exists("idGroupe", $donnees) ? filter_var($donnees["idGroupe"], FILTER_VALIDATE_INT) : null;
-
-    if($nom != null && $email == null && $motPasse != null && $remediation != null && $idGroupe != null){
-
-        $user = $userCtrl->createMember($nom, $motPasse, $remediation, $idGroupe);
-
-        echo(json_encode($user));
-
-        if(is_array($user)){
-
-            http_response_code(INCOMPLET);
-            die();
-
-        }
-        else{
-
-            http_response_code(CREE_RESSOURCE);
-            die();
-
-        }
-
-    }
-    else if($nom != null && $email != null && $motPasse != null && $remediation != null && $idGroupe == null) {
-
-        $user = $userCtrl->createAdmin($nom, $email, $motPasse, $remediation, $groupeCtrl);
-
-        echo(json_encode($user));
-
-        if(is_array($user)){
-
-            http_response_code(INCOMPLET);
-            die();
-
-        }
-        else{
-
-            http_response_code(CREE_RESSOURCE);
-            die();
-
-        }
-
-    }
-    else{
-
-        echo(json_encode(["data" => "Vous n'avez par fournie le nombre d'information nécessaire pour créer un utilisateur."]));
-        http_response_code(INCOMPLET);
         die();
 
     }
---
+++ backend/api/register/index.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * 
+ * Project : Budget manager
+ * Description : Une application de gestion de budget et de dépenses monétaire.
+ * File : register/index.php
+ * Authors : Jérémy Ballanfat, Illya Nuzhny
+ * Date : 29 avril 2024
+ * 
+ */
+
+header('Content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+require_once("../php/constantes.php");
+require_once("../php/fonction.php");
+require_once("../../../vendor/autoload.php");
+require_once("../../secret.php");
+
+use Projet\Budgetmanager\api\php\controller\UserCtrl;
+use Projet\Budgetmanager\api\php\controller\GroupeCtrl;
+
+$error = [];
+$userCtrl = new UserCtrl();
+$groupeCtrl = new GroupeCtrl();
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $nom = "";
+    $email = "";
+    $motPasse = "";
+    $remediation = 0;
+    $idGroupe = 0;
+
+    $donnees = recuperDonner();
+
+    $nom = array_key_exists("nom", $donnees) ? filter_var($donnees["nom"], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null;
+    $email = array_key_exists("email", $donnees) ? filter_var($donnees["email"], FILTER_VALIDATE_EMAIL) : null;
+    $motPasse = array_key_exists("motPasse", $donnees) ? filter_var($donnees["motPasse"], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : null;
+    $remediation = array_key_exists("remediation", $donnees) ? filter_var($donnees["remediation"], FILTER_VALIDATE_INT) : null;
+    $idGroupe = array_key_exists("idGroupe", $donnees) ? filter_var($donnees["idGroupe"], FILTER_VALIDATE_INT) : null;
+
+    if($nom != null && $email == null && $motPasse != null && $remediation != null && $idGroupe != null){
+
+        $user = $userCtrl->createMember($nom, $motPasse, $remediation, $idGroupe);
+
+        echo(json_encode($user));
+
+        if(is_array($user)){
+
+            http_response_code(INCOMPLET);
+            die();
+        }
+        else{
+
+            http_response_code(CREE_RESSOURCE);
+            die();
+
+        }
+
+    }
+    else if($nom != null && $email != null && $motPasse != null && $remediation != null && $idGroupe == null) {
+
+        $user = $userCtrl->createAdmin($nom, $email, $motPasse, $remediation, $groupeCtrl);
+
+        echo(json_encode($user));
+
+        if(is_array($user)){
+
+            http_response_code(INCOMPLET);
+            die();
+
+        }
+        else{
+
+            http_response_code(CREE_RESSOURCE);
+            die();
+
+        }
+
+    }
+    else{
+
+        echo(json_encode(["data" => "Vous n'avez par fournie le nombre d'information nécessaire pour créer un utilisateur."]));
+        http_response_code(INCOMPLET);
+        die();
+
+    }
+
+}
+else{
+
+    http_response_code(SERVEUR_PROBLEME);
+    die();
+
+}
